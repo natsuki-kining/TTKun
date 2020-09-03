884324f5fea6043af25320266f4125302f4d8969
natsuki_kinig <natsuki_kining@126.com>
2020-09-03 23:40:22 +0800

    去除尾行注释

--- src/main/java/com/natsuki_kining/ttkun/crawler/core/request/type/http/HtmlUnitType.java
+++ src/main/java/com/natsuki_kining/ttkun/crawler/core/request/type/http/HtmlUnitType.java
@@ -27,16 +27,21 @@ public class HtmlUnitType extends AbstractRequestType {
         String result = "";
 
         final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-
-        webClient.getOptions().setThrowExceptionOnScriptError(false);//当JS执行出错的时候是否抛出异常
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);//当HTTP的状态非200时是否抛出异常
+        //当JS执行出错的时候是否抛出异常
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        //当HTTP的状态非200时是否抛出异常
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.getOptions().setActiveXNative(false);
-        webClient.getOptions().setCssEnabled(false);//是否启用CSS
-        webClient.getOptions().setJavaScriptEnabled(true); //很重要，启用JS
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());//很重要，设置支持AJAX
-
-        webClient.getOptions().setTimeout(request.getTimeout());//设置“浏览器”的请求超时时间
-        webClient.setJavaScriptTimeout(request.getTimeout());//设置JS执行的超时时间
+        //是否启用CSS
+        webClient.getOptions().setCssEnabled(false);
+        //很重要，启用JS
+        webClient.getOptions().setJavaScriptEnabled(true);
+        //很重要，设置支持AJAX
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        //设置“浏览器”的请求超时时间
+        webClient.getOptions().setTimeout(request.getTimeout());
+        //设置JS执行的超时时间
+        webClient.setJavaScriptTimeout(request.getTimeout());
         if (request.getHeaders() != null) {
             for (String key : request.getHeaders().keySet()) {
                 webClient.addRequestHeader(key, request.getHeaders().get(key));
@@ -55,7 +60,8 @@ public class HtmlUnitType extends AbstractRequestType {
             webClient.close();
             log.error(e.getMessage(), e);
         }
-        webClient.waitForBackgroundJavaScript(request.getWaitForBackgroundJavaScript());//该方法阻塞线程
+        //该方法阻塞线程
+        webClient.waitForBackgroundJavaScript(request.getWaitForBackgroundJavaScript());
 
         result = page.asXml();
         webClient.close();
